aa50217624d2194d010b123cb6ddcf7cd6b3d969
MERU <115197065+merucode@users.noreply.github.com>
2023-08-12 21:25:14 +0900

    Update

--- gym-examples/gym_examples/__init__.py
+++ gym-examples/gym_examples/__init__.py
@@ -1,7 +1,7 @@
 from gym.envs.registration import register
 
 register(
-    id='gym_examples/GridWorld-v0',
-    entry_point='gym_examples.envs:GridWorldEnv',
+    id='TradeWorld-v0',
+    entry_point='gym_examples.envs:TradeWorldEnv',
     max_episode_steps=300,
 )
--- gym-examples/gym_examples/envs/__init__.py
+++ gym-examples/gym_examples/envs/__init__.py
@@ -1,2 +1,1 @@
-
-from gym_examples.envs.grid_world import GridWorldEnv
+from gym_examples.envs.trade_world import TradeWorldEnv
